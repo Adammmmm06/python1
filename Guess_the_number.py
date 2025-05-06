eaa2ad1ce421295d8531ea4bba4edbf70b849401
Adam Blennow <adammbappe06@gmail.com>
2025-05-06 13:53:14 +0200

    Jag har jobbat lite mer på gissa numret uppgiften.

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -5,3 +5,8 @@ print("Jag tänker på ett tal mellan 1 och 3")
 
 guess = int(input("Gissa vilket tal jag tänker på!"))
 
+if guess == random_number:
+  print("Snyggt jobbat! Det var rätt!")
+else:
+  print("Tyvärr, det var fel.")
+
